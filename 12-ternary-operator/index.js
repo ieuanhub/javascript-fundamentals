915657fd5d349540dd71435f3edd2bceeeff23c9
ieuanhub <imorris2005@hotmail.com>
2026-09-14 16:04:23 +0100

    completed stage twelve - ternary operator
    got introduced to ternary using the ? command which makes life so much easier from having to use else if and if all the time, you can see four examples has been shown with the last one relating to what happens if a store includes a discount to an item

---
+++ 12-ternary-operator/index.js
@@ -0,0 +1,23 @@
+// ternary operator = a shortcut to if{} and else{} statements
+//                    helps to assign a variable based on a condition
+//                    condition ? codeIfTrue : codeIfFalse;
+
+// Example 1
+// let age = 21;
+// let message = age >= 18 ? "You're an adult" : "You're a minor";
+// console.log(message);
+
+// Example 2
+// let time = 14;
+// let greeting = time < 12 ? "Good morning!" : "Good afternoon!";
+// console.log(greeting);
+
+// Example 3
+// let isStudent = true;
+// let message = isStudent ? "You're a student!" : "You're NOT a student!";
+// console.log(message);
+
+// Example 4
+let purchaseAmount = 125;
+let discount = purchaseAmount >= 100 ? 10 : 0;
+console.log(`Your total is £${purchaseAmount - purchaseAmount * discount / 100}`)
